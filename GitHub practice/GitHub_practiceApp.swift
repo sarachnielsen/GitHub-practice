c3444c9e641d183aca882d6d39e482123c523af4
Sara Nielsen <sara.c.h.nielsen@gmail.com>
2023-06-12 09:00:31 -0700

    Initial Commit

---
+++ GitHub practice/GitHub_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_practiceApp.swift
+//  GitHub practice
+//
+//  Created by Sara Nielsen on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
